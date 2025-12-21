0b475866c30906365e3d7cbc8275a15c15cc5ed4
Yuri Gabriel <yuritj367@gmail.com>
2025-12-21 10:52:10 -0300

    Add files via upload
     Faça um programa que leia as duas notas de um aluno em uma matéria e mostre
    na tela a sua média na disciplina.
    Ex:
    Nota 1: 4.5
    Nota 2: 8.5
    A média entre 4.5 e 8.5 é igual a 6.5

---
+++ PASSO 01 - SEQUÊNCIAS/Ex-05.py
@@ -0,0 +1,4 @@
+Nota1 = float(input("informe a primeira nota:"))
+Nota2 = float(input("informe a segunda nota:"))
+media = (Nota1 + Nota2) / 2
+print(f"A média entre {Nota1} e {Nota2} é igual a {media}")
