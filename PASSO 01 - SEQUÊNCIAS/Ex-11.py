4ff3a94a8bb8d793c2f187ee7c384f46b3af8ee1
Yuri Gabriel <yuritj367@gmail.com>
2026-01-07 12:10:55 -0300

    Add files via upload
    Desenvolva uma lógica que leia os valores de A, B e C de uma equação do
    segundo grau e mostre o valor de Delta.

---
+++ PASSO 01 - SEQUÊNCIAS/Ex-11.py
@@ -0,0 +1,7 @@
+A = float(input("Digite o valor de A: "))
+B = float(input("Digite o valor de B: "))
+C = float(input("Digite o valor de C: "))
+
+delta = B**2 - 4*A*C
+
+print(f"O valor de Delta é:{delta}", )
