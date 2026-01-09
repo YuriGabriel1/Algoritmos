f921c3544391d83255e28370b312ab25aef32c6f
Yuri Gabriel <yuritj367@gmail.com>
2026-01-09 13:30:28 -0300

    Add files via upload
     Faça um algoritmo que leia o salário de um funcionário, calcule e mostre o
    seu novo salário, com 15% de aumento.

---
+++ PASSO 01 - SEQUÊNCIAS/Ex-13.py
@@ -0,0 +1,6 @@
+salario = float(input("Digite o salário do funcionário: "))
+
+aumento = salario * 0.15
+novo_salario = salario + aumento
+
+print(f"Novo salário com 15% de aumento: R$ {novo_salario:.2f}")
