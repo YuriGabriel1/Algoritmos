99d33fab460a51d2c5dab79b40bae4e6f8bcf7c0
Yuri Gabriel <yuritj367@gmail.com>
2026-01-11 13:38:07 -0300

    Add files via upload
    Crie um programa que leia o número de dias trabalhados em um mês e mostre o
    salário de um funcionário, sabendo que ele trabalha 8 horas por dia e ganha R$25
    por hora trabalhada.

---
+++ PASSO 01 - SEQUÊNCIAS/Ex-15.py
@@ -0,0 +1,5 @@
+dias_trabalhados = int(input("Digite o número de dias trabalhados no mês: "))
+horas_por_dia = 8
+valor_hora = 25
+salario = dias_trabalhados * horas_por_dia * valor_hora
+print(f"O salário do funcionário é R${salario:.2f}")
