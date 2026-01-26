66391c3738802ebc02f49ada715cb501268cedb5
Yuri Gabriel <yuritj367@gmail.com>
2026-01-26 15:51:11 -0300

    Crie um programa que leia duas notas de um aluno e calcule a sua média, mostrando uma mensagem no final, de acordo com a média atingida:  - Média até 4.9: REPROVADO  - Média entre 5.0 e 6.9: RECUPERAÇÃO  - Média 7.0 ou superior: APR

--- PASSO 03 - CONDIÇÕES COMPOSTAS/Ex-27.py
+++ PASSO 03 - CONDIÇÕES COMPOSTAS/Ex-27.py
@@ -1,2 +1,12 @@
+from time import sleep
 nota1 = float(input("Informe a primeira nota:"))
+sleep(1)
 nota2 = float(input("Informe a segunda nota:"))
+sleep(1)
+media = (nota1 + nota2)/2
+if 5.0 <= media <= 6.9:
+    print("Recuperação!")
+elif media <=4.9:
+    print("Reprovado")
+elif media >=7:
+    print("Aprovado")
