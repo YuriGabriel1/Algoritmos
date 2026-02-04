bfb7e32d51ed9133710e4162b0577e470c7ab34b
Yuri Gabriel <yuritj367@gmail.com>
2026-02-04 15:55:23 -0300

    Crie um jogo de JoKenPo (Pedra-Papel-Tesoura)s

---
+++ PASSO 03 - CONDIÇÕES COMPOSTAS/Ex-31.py
@@ -0,0 +1,31 @@
+import random
+
+print("=== JOKENPÔ ===")
+print("1 - Pedra")
+print("2 - Papel")
+print("3 - Tesoura")
+
+jogador = int(input("Escolha uma opção: "))
+
+computador = random.randint(1, 3)
+
+if computador == 1:
+    print("Computador escolheu Pedra")
+    print(f"Jogador escolheu {jogador}")
+elif computador == 2:
+    print("Computador escolheu Papel")
+    print(f"Jogador escolheu {jogador}")
+else:
+    print("Computador escolheu Tesoura")
+    print(f"Jogador escolheu {jogador}")
+if jogador == computador:
+    print(f"O computador escolheu o mesmo do jogador{computador}")
+    print(f"Jogador escolheu {jogador}")
+    print("Empate!")
+elif (jogador == 1 and computador == 3) or \
+     (jogador == 2 and computador == 1) or \
+     (jogador == 3 and computador == 2):
+    print("Você ganhou! 🎉")
+
+else:
+    print("Computador ganhou 😢")
